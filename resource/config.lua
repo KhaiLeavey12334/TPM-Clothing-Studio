68632a5cf1a517235159e379e608dd37d9e1e773
KhaiLeavey12334 <88949232+KhaiLeavey12334@users.noreply.github.com>
2026-09-12 18:13:17 -0400

    Alpha 0.0.3 add clothing config

--- resource/config.lua
+++ resource/config.lua
@@ -2,11 +2,15 @@ Config = Config or {}
 
 Config.Debug = true
 Config.ResourceName = 'TPM Clothing Studio'
-Config.Version = '0.0.2-alpha'
+Config.Version = '0.0.3-alpha'
 
 Config.Commands = {
     openStudio = 'tpmstudio',
-    toggleCamera = 'tpmcamera'
+    toggleCamera = 'tpmcamera',
+    nextDrawable = 'tpmnextdrawable',
+    previousDrawable = 'tpmprevdrawable',
+    nextTexture = 'tpmnexttexture',
+    previousTexture = 'tpmprevtexture'
 }
 
 Config.Nui = {
@@ -62,5 +66,35 @@ Config.Camera = {
             distance = 1.25,
             heading = 180.0
         }
+    }
+}
+
+Config.Clothing = {
+    defaultComponent = 11,
+    defaultProp = 0,
+    components = {
+        { id = 0, key = 'face', label = 'Face' },
+        { id = 1, key = 'mask', label = 'Mask' },
+        { id = 2, key = 'hair', label = 'Hair' },
+        { id = 3, key = 'arms', label = 'Arms' },
+        { id = 4, key = 'legs', label = 'Legs' },
+        { id = 5, key = 'bags', label = 'Bags' },
+        { id = 6, key = 'shoes', label = 'Shoes' },
+        { id = 7, key = 'accessories', label = 'Accessories' },
+        { id = 8, key = 'undershirt', label = 'Undershirt' },
+        { id = 9, key = 'body_armor', label = 'Body Armor' },
+        { id = 10, key = 'decals', label = 'Decals' },
+        { id = 11, key = 'tops', label = 'Tops' }
+    },
+    props = {
+        { id = 0, key = 'hats', label = 'Hats' },
+        { id = 1, key = 'glasses', label = 'Glasses' },
+        { id = 2, key = 'ears', label = 'Ear Pieces' },
+        { id = 6, key = 'watches', label = 'Watches' },
+        { id = 7, key = 'bracelets', label = 'Bracelets' }
+    },
+    collections = {
+        enabled = true,
+        fallback = 'base'
     }
 }
