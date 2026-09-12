5e6374059d88738565513159ad3ed3821e9d7e4e
KhaiLeavey12334 <88949232+KhaiLeavey12334@users.noreply.github.com>
2026-09-12 16:12:42 -0400

    Alpha 0.0.1 logger

---
+++ resource/client/core/logger.lua
@@ -0,0 +1,39 @@
+Studio.Logger = Studio.Logger or {}
+
+local levels = {
+    debug = '^5DEBUG^7',
+    info = '^2INFO^7',
+    warn = '^3WARN^7',
+    error = '^1ERROR^7'
+}
+
+local function timestamp()
+    if not Config.Logging.showTimestamps then
+        return ''
+    end
+
+    return os.date('[%H:%M:%S] ')
+end
+
+local function write(level, message)
+    local label = levels[level] or levels.info
+    print(('%s[%s] %s %s'):format(timestamp(), Config.Logging.prefix, label, message))
+end
+
+function Studio.Logger.Debug(message)
+    if Config.Debug then
+        write('debug', message)
+    end
+end
+
+function Studio.Logger.Info(message)
+    write('info', message)
+end
+
+function Studio.Logger.Warn(message)
+    write('warn', message)
+end
+
+function Studio.Logger.Error(message)
+    write('error', message)
+end
