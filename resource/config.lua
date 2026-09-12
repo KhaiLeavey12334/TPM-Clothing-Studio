1ac619f98b422f9febff6fd931c3a6e29bc58514
KhaiLeavey12334 <88949232+KhaiLeavey12334@users.noreply.github.com>
2026-09-12 16:12:38 -0400

    Alpha 0.0.1 resource config

---
+++ resource/config.lua
@@ -0,0 +1,18 @@
+Config = Config or {}
+
+Config.Debug = true
+Config.ResourceName = 'TPM Clothing Studio'
+Config.Version = '0.0.1-alpha'
+
+Config.Commands = {
+    openStudio = 'tpmstudio'
+}
+
+Config.Nui = {
+    defaultVisible = false
+}
+
+Config.Logging = {
+    prefix = 'TPM Clothing Studio',
+    showTimestamps = true
+}
